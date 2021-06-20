b2a123dd150785ce96218e3f1b4fbe2324ddfa6a
AFSIN ASIF <afsnasif@gmail.com>
2021-06-20 18:13:33 +0530

    loop and desicion

--- javascript works/arrays/binarysearch.js
+++ javascript works/arrays/binarysearch.js
@@ -1,5 +1,27 @@
-var arr=[9,2,7,8,3];
+var arr=[8,7,11,12,10,9];
 //arr.sort((num1,num2)=>num1<num2? -1:1)
 
 arr.sort((num1,num2)=>num1-num2)
 console.log(arr);
+//arr.sort((num1,num2)=>num2-num1)
+//console.log(arr);
+
+var element=12;
+var flag=0;
+var low=0,upp=arr.length-1;
+while(low<=upp){
+    let mid=Math.floor((low+upp)/2)
+
+    if(element>arr[mid]){
+        low=mid+1;
+    }
+    else if(element<arr[mid]){
+        upp=mid-1;
+    }
+    else if(element==arr[mid]){
+        flag++;
+        break;
+    }
+    
+}
+console.log(flag==0?"not found":"found");
---
+++ javascript works/functions/function_demo.js
@@ -0,0 +1,10 @@
+//
+function add(num1,num2) {
+    let res=num1+num2;
+    return res;
+}
+
+var res=add(5,10);
+console.log(res);
+
+
